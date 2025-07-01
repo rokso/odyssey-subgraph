bdb22167ade8eb1d0802c4880105e94238bf24d1
Rohit Solia <rohit.solia@gmail.com>
2025-07-01 08:55:58 -0500

    Updated dependencies and added custom multichain support

---
+++ config/base/address.ts
@@ -0,0 +1,4 @@
+import { Address } from '@graphprotocol/graph-ts'
+
+export const POSITION_REGISTRY = Address.fromString('0xeE156D8ea7b96a5524CcC3CF9283ab85E80E9534')
+export const MASTER_ORACLE = Address.fromString('0x99866a6074ADb027f09c9AF31929dB5941D36DA7')
---
+++ config/ethereum/address.ts
@@ -0,0 +1,4 @@
+import { Address } from '@graphprotocol/graph-ts'
+
+export const POSITION_REGISTRY = Address.fromString('0xeE156D8ea7b96a5524CcC3CF9283ab85E80E9534')
+export const MASTER_ORACLE = Address.fromString('0x80704Acdf97723963263c78F861F091ad04F46E2')
